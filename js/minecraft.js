a620f15f84d5d0f949089e4ffdea8d5eb3e714c6
Jddanan <jddanan@gmail.com>
2018-11-18 16:50:21 +0200

    adding board in js

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
 });
+
+var Minecraft = {};
+Minecraft.board = [
+    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky",],
+    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky",],
+    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky",],
+    ["sky","sky","cloud","cloud","sky","sky","sky","sky","sky","sky","sky","sky",],
+    ["sky","sky","cloud","cloud","cloud","cloud","sky","sky","sky","sky","sky","sky",],
+    ["sky","sky","sky","cloud","sky","sky","sky","sky","sky","sky","sky","sky",],
+    ["sky","sky","sky","sky","sky","sky","sky","","leaf","sky","sky","sky",],
+    ["sky","sky","sky","sky","sky","sky","leaf","","tree","leaf","sky","sky",],
+    ["sky","sky","sky","sky","sky","sky","sky","sky","tree","sky","sky","sky",],
+    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","tree","dirt","dirt","dirt",],
+    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt",],
+    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt",]
+];
