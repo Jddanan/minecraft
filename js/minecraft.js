7354ac63461c558a1367ffe2364c4bf104ec4c4d
JrmSK <jeremy.skornik@gmail.com>
2018-11-19 00:54:30 +0200

    World is now generated  in generateWorld() // css MVP for pixels

--- js/minecraft.js
+++ js/minecraft.js
@@ -3,17 +3,38 @@ $(document).ready(function () {
 });
 
 var Minecraft = {};
-Minecraft.board = [
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky",],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky",],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky",],
-    ["sky","sky","cloud","cloud","sky","sky","sky","sky","sky","sky","sky","sky",],
-    ["sky","sky","cloud","cloud","cloud","cloud","sky","sky","sky","sky","sky","sky",],
-    ["sky","sky","sky","cloud","sky","sky","sky","sky","sky","sky","sky","sky",],
-    ["sky","sky","sky","sky","sky","sky","sky","","leaf","sky","sky","sky",],
-    ["sky","sky","sky","sky","sky","sky","leaf","","tree","leaf","sky","sky",],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","tree","sky","sky","sky",],
-    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","tree","dirt","dirt","dirt",],
-    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt",],
-    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt",]
+
+/* HARDCODED board */
+Minecraft.world = [
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "cloud", "cloud", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "cloud", "cloud", "cloud", "cloud", "sky", "sky", "sky", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "cloud", "sky", "sky", "leaf", "sky", "leaf", "sky", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "leaf", "leaf", "leaf", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "leaf", "leaf", "tree", "leaf", "sky", "sky",],
+    ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "tree", "sky", "sky", "sky",],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "tree", "dirt", "dirt", "dirt",],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",],
+    ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt",]
 ];
+
+/* Generates the world from HARDCODED board */
+Minecraft.generateWorld = function (world) {
+
+    for (var i = 0; i < world.length; i++) {
+        $(`#world`).append(`<div class='row justify-content-center'>`);
+        for (var j = 0; j < world[0].length; j++) {
+            $(`.row:nth-child(${i})`).append(`<div class='pixel ${Minecraft.world[i][j]}'>`);
+        }
+    }
+}
+
+/* Initiates the game */
+Minecraft.start = function() {
+
+    Minecraft.generateWorld(Minecraft.world)
+}
+
+Minecraft.start();
